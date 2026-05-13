6e1f29d69b40c29e1c7b70e4b13816fa3d9b2af8
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-05-13 17:34:28 +0300

    Create OverseerPythonScript.py

---
+++ Python/OverseerPythonScript.py
@@ -0,0 +1,51 @@
+import sys, os, http.client, ipaddress, json, socket
+if len(sys.argv) == 3:
+  arg, mode = sys.argv[1], sys.argv[2]
+
+  if mode == "ip" or mode == "ipaddress":
+    ip, hsot = ipaddress.ip_address(arg), None
+    ipinfo = http.client.HTTPSConnection("ipinfo.io"); ipinfo.request("GET", "/json"); ipinfo = json.loads(ipinfo.getresponse().read())
+    
+    try:
+       hsot = str(socket.gethostbyaddr(ip)[0])
+    except Exception as en:
+       print(en)
+
+    print("Информацию об айпи адресе " + arg + ": ")
+    print(f"   Является приватным: {"да" if ip.is_private == True else "нет"}")
+    print(f"   Является глобальным: {"да" if ip.is_global == True else "нет"}")
+    print(f"   Предполагаемое местоположение: {"city" in ipinfo and (ipinfo["country"] + " " + ipinfo["city"] + " " + ipinfo["region"]) or "не известно"}")
+    print(f"   ASN: {"org" in ipinfo and ipinfo["org"] or "не найдено"}")
+    print(f"   Локация: {"loc" in ipinfo and ipinfo["loc"] or "не найдено"}")
+    print(f"   Имеет домен: {hsot != None and "да (" + hsot + ")" or "нет"}
+    print(f"   CGNAT: {ip._constants._public_network}")
+    print(f"   LLA: {ip._constants._linklocal_network}")
+    
+  elif mode == "domain" or mode == "host":
+    amam, server, asninfo = socket.gethostbyname(arg), None, ""
+    
+    try:
+       req = http.client.HTTPSConnection("rdap.db.ripe.net")
+       req.request("GET", f"/ip/{arg}")
+       response = json.loads(req.getresponse().read())
+       asninfo = f""'
+      Имя провайдера: {response["name"]}
+      Тип провайдера: {response["type"]}
+       """
+    except Exception as am:
+       print(am)
+    
+    try:
+       req = http.client.HTTPSConnection(arg)
+       req.request("OPTIONS", "/")
+       server = req.getresponse().getheader("Server")
+       req.close()
+    except Exception as am:
+       print(am)
+    
+    print("Информация об домене " + arg + ": ")
+    print(f"   Айпи адрес сайта: {amam}")
+    print(f"   Домен сайта (FQDN): {socket.getfqdn(amam)}")
+    print(f"   CIDR: {ipaddress.ip_network(amam).supernet()}")
+    print(f"   Сервер: {server}")
+    print(asninfo)
