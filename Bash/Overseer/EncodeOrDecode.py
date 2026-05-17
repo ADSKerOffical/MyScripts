0861c14cf7d60d6742bf7bd028d2f2306e0a13af
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-05-17 04:21:47 +0300

    Update EncodeOrDecode.py

--- Bash/Overseer/EncodeOrDecode.py
+++ Bash/Overseer/EncodeOrDecode.py
@@ -1,4 +1,4 @@
-import base64, codecs, binascii, hashlib, urllib.parse, sys, pathlib, os
+import base64, codecs, binascii, hashlib, urllib.parse, sys, pathlib, os, re
 
 method = sys.argv[1].lower()
 stri = sys.argv[2]
@@ -24,9 +24,14 @@ if method == "hex":
            rs = stri.encode("utf-8").hex()
 elif method == "bytes" or method == "ascii":
     if decrypt == True:
-           rs = stri.encode("ascii")
+           rs = stri.encode("utf-8", errors="ignore")
     else:
-           rs = stri.encode("utf-8")
+           rs = stri.encode("ascii")
+elif method == "decimal_bytes":
+    if decrypt == True:
+           rs = re.sub(r'\\(\d{1,3})', lambda m: chr(int(m.group(1))), stri)
+    else:
+           rs = ''.join(f'\\{ord(c)}' for c in stri)
 elif method == "base64" or method == "b64":
     if decrypt == True:
            rs = base64.b64decode(stri).decode("utf-8")
@@ -41,8 +46,19 @@ elif method == "md5":
            rs = hashlib.md5(stri.encode()).hexdigest()
 elif method == "sha256":
            rs = hashlib.sha256(stri.encode()).hexdigest()
+elif method == "rot13" or method == "rot-13":
+    if decrypt == True:
+           rs = codecs.decode(stri, "rot-13")
+    else:
+           rs = codecs.encode(stri, "rot-13")
 else:
-   print("Unknown method for string")
-   os._exit(0)
+    try:
+       if decrypt == True:
+          rs = codecs.decode(stri, method)
+       else:
+          rs = codecs.encode(stri, method)
+    except Exception:
+       print(f"Формата {method} не существует")
+
 print(rs)
 save(rs)
