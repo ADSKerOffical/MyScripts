9875301f9b8cf68c42227883eafeb367cb51f2bf
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-05-17 04:49:28 +0300

    Update dnsinfo.js

--- Bash/Overseer/dnsinfo.js
+++ Bash/Overseer/dnsinfo.js
@@ -2,26 +2,26 @@ const domain = process.argv[2]
 let dns = require("dns")
 
 dns.resolveMx(domain, (err, mail) => {
-   if (err) { console.log("  MX: не обнаружен"); return err };
+   if (err) { console.log("    MX: не обнаружен"); return err };
    console.log("MX: ", mail.map(r => r.exchange).values().next().value);
 });
 
 dns.resolve6(domain, (err, ips) => {
-  if (err) { console.log("  AAAA: не обнаружен"); return err };
+  if (err) { console.log("    AAAA: не обнаружен"); return err };
   console.log("AAAA: ", ips[0])
 });
 
 dns.resolveNs(domain, (err, mail) => {
-   if (err) { console.log("  NS: не обнаружен"); return err };
+   if (err) { console.log("    NS: не обнаружен"); return err };
    console.log("NS: ", mail[0]);
 });
 
 dns.resolveSoa(domain, (err, mail) => {
-   if (err) { console.log("  SOA: не обнаружен"); return err };
+   if (err) { console.log("    SOA: не обнаружен"); return err };
    console.log("SOA: ", mail["hostmaster"]);
 });
 
 dns.resolveTxt(domain, (err, mail) => {
-  if (err) { console.log("  TXT: не обнаружен"); return err };
+  if (err) { console.log("    TXT: не обнаружен"); return err };
   console.log("TXT: ", mail)
 });
