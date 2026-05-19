a4782909e17864206251e967cb99774777c2d4d8
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-05-19 23:28:24 +0300

    Update PyScript.py

--- Bash/Overseer/PyScript.py
+++ Bash/Overseer/PyScript.py
@@ -1,4 +1,4 @@
-import sys, os, http.client, ipaddress, json, socket
+import sys, os, http.client, ipaddress, json, socket, ssl
 if len(sys.argv) == 3:
   arg, mode = sys.argv[1], sys.argv[2]
 
@@ -35,11 +35,29 @@ if len(sys.argv) == 3:
     except Exception as am:
        pass
     
+    newinfo = {}
+    
     try:
        req = http.client.HTTPSConnection(arg)
-       req.request("OPTIONS", "/")
+       req.request("GET", "/")
        server = req.getresponse().getheader("Server")
        req.close()
+    except Exception as am:
+       pass
+       
+    try:
+       context = ssl.create_default_context()
+       with socket.create_connection((arg, 443)) as sock:
+          with context.wrap_socket(sock, server_hostname=arg) as ssock:
+             request = f"GET / HTTP/1.1\r\nHost: {arg}\r\nConnection: close\r\n\r\n"
+             ssock.send(request.encode())
+             
+             peer = ssock.getpeercert()
+             newinfo["s_ver"] = ssock.version()
+             newinfo["alpn"] = str(ssock.selected_alpn_protocol())
+             newinfo["cipher"] = ssock.cipher()
+             newinfo["subject"] = peer.get("subject")
+             newinfo["issuer"] = peer.get("issuer")
     except Exception as am:
        pass
     
@@ -48,6 +66,12 @@ if len(sys.argv) == 3:
     print(f"   Домен сайта (FQDN): {socket.getfqdn(amam)}")
     print(f"   CIDR: {ipaddress.ip_network(amam).supernet()}")
     print(f"   Сервер: {server}")
+    
+    if "s_ver" in newinfo:
+       print(f"   Версия сокета: {newinfo["s_ver"]}")
+       print(f"   ALPN: {newinfo["alpn"] is None and "не обнаружено" or newinfo["alpn"]}")
+       print(f"   Шифр: {newinfo["cipher"]}")
+       print(f"   Субъект: {newinfo["subject"]}")
+       print(f"   Издатель: {newinfo["issuer"]}")
+    
     print(asninfo)
-  elif mode == "self":
-    pass
