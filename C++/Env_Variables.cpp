f45808292521749423cb5516ed1a99636d3eafb7
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-06-14 16:54:30 +0300

    Update Env_Variables.cpp

--- C++/Env_Variables.cpp
+++ C++/Env_Variables.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char* argv[], char* envp[]) {
-    // Цикл по всем переменным окружения, переданным процессу
+    // Можно получить значение конкретной переменной
+    const char* storage = std::getenv("EXTERNAL_STORAGE");
+    const char* pwd = std::getenv("PWD");
+    std::cout << storage << "\n"; // в моём случае /sdcard
+    std::cout << pwd << "\n"; // путь к home
+    
+    // Цикл по всем переменным окружения
     for (char** env = envp; *env != nullptr; ++env) {
         std::cout << *env << std::endl;
     }
